584865402c1286d9d9a27ce3eafb7f7db2866ee2
David Parker <git@nofuture.org.uk>
2014-04-17 18:59:14 +0100

    Add copyright statement

--- samples/psutil/iot-psutil.py
+++ samples/psutil/iot-psutil.py
@@ -1,3 +1,15 @@
+# *****************************************************************************
+# Copyright (c) 2014 IBM Corporation and other Contributors.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html 
+#
+# Contributors:
+#   IBM - Initial Contribution
+# *****************************************************************************
+
 import getopt
 import time
 import threading
