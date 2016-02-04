f1bb87a5428f7a1e1c3f784b68d61c9c179691b2
durera <durera@nofuture.org.uk>
2016-02-04 12:52:33 +0000

    Bump version to 0.1.8, update changes.txt

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='ibmiotf',
-    version="0.1.7",
+    version="0.1.8",
     author='David Parker',
     author_email='parkerda@uk.ibm.com',
     package_dir={'': 'src'},
