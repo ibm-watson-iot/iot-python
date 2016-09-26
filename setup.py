c081ffe247c8976adf7cde576b21ef3405bcade2
David Parker <durera@nofuture.org.uk>
2016-09-26 12:02:22 +0100

    Updates in preparation for 0.2.5 release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='ibmiotf',
-    version="0.2.4",
+    version="0.2.5",
     author='David Parker',
     author_email='parkerda@uk.ibm.com',
     package_dir={'': 'src'},
