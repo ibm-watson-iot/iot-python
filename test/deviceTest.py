dcbbe5b9dcbca0bca7ce125fef8098dc85fde54f
Lokesh K Haralakatta <lokesh.h.k@in.ibm.com>
2017-02-09 16:47:55 +0530

    Skipping publishOverHTTPs test as it needs CSC

--- test/deviceTest.py
+++ test/deviceTest.py
@@ -151,6 +151,7 @@ class TestDevice:
         assert_true(self.deviceClient.publishEvent("testPublishXMLEvent", "xml", myData,on_publish=devPublishCallback,qos=2))
         self.deviceClient.disconnect()
 
+    @SkipTest
     def testPublishEventOverHTTPs(self):
         myData={'name' : 'foo', 'cpu' : 60, 'mem' : 50}
         assert_equals(self.httpClient.publishEvent("testPublishEventHTTPs", myData),200)
