3822b5b142d54f83aadf7e366f2b5b925f557e1a
David Parker <parkerda@uk.ibm.com>
2018-05-31 13:24:46 +0100

    Make tests throw better error if env vars are missing

--- test/testUtils/__init__.py
+++ test/testUtils/__init__.py
@@ -7,5 +7,12 @@ class AbstractTest(object):
     WIOTP_API_TOKEN=os.getenv("WIOTP_API_TOKEN")
     ORG_ID = os.getenv("WIOTP_ORG_ID")
     
+    if WIOTP_API_KEY is None:
+        raise Exception("WIOTP_API_KEY environment variable is not set")
+    if WIOTP_API_TOKEN is None:
+        raise Exception("WIOTP_API_TOKEN environment variable is not set")
+    if ORG_ID is None:
+        raise Exception("WIOTP_ORG_ID environment variable is not set")
+    
     appOptions = {'auth-key': WIOTP_API_KEY, 'auth-token': WIOTP_API_TOKEN}
     setupAppClient = ibmiotf.application.Client(appOptions)
