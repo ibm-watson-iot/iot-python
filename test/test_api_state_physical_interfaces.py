d87582ce294e58c4bc8d6dbb4f0bc04f5852f57a
P. D. Stone <stonepd@uk.ibm.com>
2019-06-03 16:54:58 +0100

    Fix PI test

--- test/test_api_state_physical_interfaces.py
+++ test/test_api_state_physical_interfaces.py
@@ -16,6 +16,7 @@ from wiotp.sdk.exceptions import ApiException
 import string
 import json
 
+@testUtils.oneJobOnlyTest
 class TestPhysicalInterfaces(testUtils.AbstractTest):
     
     testSchemaName = "python-api-test-pi_schema"
