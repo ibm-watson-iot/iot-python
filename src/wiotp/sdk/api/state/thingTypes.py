7f4149cc57d650d7fc3f9a3617824b62d43b71a8
P. D. Stone <stonepd@uk.ibm.com>
2019-10-28 13:32:31 +0000

    Code tidy

--- src/wiotp/sdk/api/state/thingTypes.py
+++ src/wiotp/sdk/api/state/thingTypes.py
@@ -19,9 +19,9 @@ from wiotp.sdk.api.common import RestApiDictReadOnly
 from wiotp.sdk.api.common import RestApiModifiableProperty
 from wiotp.sdk.api.state.things import Things
 from wiotp.sdk.api.state.logicalInterfaces import BaseLogicalInterface
-        
 
-# See docs @ https://orgid.internetofthings.ibmcloud.com/docs/v0002/state-mgmt.html#/Logical Interfaces        
+
+# See docs @ https://orgid.internetofthings.ibmcloud.com/docs/v0002/state-mgmt.html#/Logical Interfaces
 class BaseThingType(RestApiItemBase):
     def __init__(self, apiClient, **kwargs):
         self._apiClient = apiClient
@@ -29,14 +29,14 @@ class BaseThingType(RestApiItemBase):
 
     # Note - data accessor functions for common data items are defined in RestApiItemBase
 
-    @property 
+    @property
     def name(self):
         return self["name"]
-    
-    @property 
+
+    @property
     def schemaId(self):
         return self["schemaId"]
-    
+
     @property
     def description(self):
         if "description" in self:
@@ -50,161 +50,156 @@ class BaseThingType(RestApiItemBase):
             return self["metadata"]
         else:
             return None
-    @property 
+
+    @property
     def schema(self):
         return self._schema
-    
-    @property 
+
+    @property
     def logicalInterfaces(self):
         return self._logicalInterfaces
 
-    @property 
+    @property
     def mappings(self):
-        return self._mappings   
- 
+        return self._mappings
+
+
 class DraftThingType(BaseThingType):
     def __init__(self, apiClient, **kwargs):
         super(DraftThingType, self).__init__(apiClient, **kwargs)
         self._url = "api/v0002/draft/thing/types/%s" % self.id
-        self._logicalInterfaces = DraftLogicalInterfaces(apiClient, self.id) # TBD need to provide access to active and draft.
+        self._logicalInterfaces = DraftLogicalInterfaces(
+            apiClient, self.id
+        )  # TBD need to provide access to active and draft.
         self._mappings = DraftMappings(apiClient, self.id)
         self._things = Things(apiClient, self.id)
 
     # Note - data accessor functions for common data items are defined in BaseDeviceType
 
-
-    @property 
+    @property
     def things(self):
-        return self._things  
-        
+        return self._things
+
     def __callDraftPatchOperation__(self, body):
         r = self._apiClient.patch(self._url, body)
-        if r.status_code in (200,202):
+        if r.status_code in (200, 202):
             return r.json()
         else:
             raise ApiException(r)
-             
+
     def activate(self):
         return self.__callDraftPatchOperation__({"operation": "activate-configuration"})
- 
+
     def validate(self):
         return self.__callDraftPatchOperation__({"operation": "validate-configuration"})
- 
+
     def differences(self):
         return self.__callDraftPatchOperation__({"operation": "list-differences"})
-    
+
+
 class ActiveThingType(BaseThingType):
     def __init__(self, apiClient, **kwargs):
         super(ActiveThingType, self).__init__(apiClient, **kwargs)
         self._url = "api/v0002/thing/types/%s" % self.id
-        self._logicalInterfaces = ActiveLogicalInterfaces(apiClient, self.id) # TBD need to provide access to active and draft.
+        self._logicalInterfaces = ActiveLogicalInterfaces(
+            apiClient, self.id
+        )  # TBD need to provide access to active and draft.
         self._mappings = ActiveMappings(apiClient, self.id)
         self._things = Things(apiClient, self.id)
 
     # Note - data accessor functions for common data items are defined in BaseThingType
 
-
-    @property 
+    @property
     def things(self):
-        return self._things  
-    
+        return self._things
+
     def __callPatchOperation__(self, body):
         r = self._apiClient.patch(self._url, body)
         if r.status_code in (200, 202):
             return r.json()
         else:
             raise ApiException(r)
-        
+
     def deactivate(self):
         return self.__callPatchOperation__({"operation": "deactivate-configuration"})
-    
 
 
 class DraftIterableThingTypeList(IterableList):
     def __init__(self, apiClient, url, filters=None):
         # This API does not support sorting
-        super(DraftIterableThingTypeList, self).__init__(
-            apiClient, DraftThingType, url, filters=filters
-        )
-        
+        super(DraftIterableThingTypeList, self).__init__(apiClient, DraftThingType, url, filters=filters)
+
+
 class ActiveIterableThingTypeList(IterableList):
     def __init__(self, apiClient, url, filters=None):
         # This API does not support sorting
-        super(ActiveIterableThingTypeList, self).__init__(
-            apiClient, ActiveThingType, url, filters=filters
-        )
+        super(ActiveIterableThingTypeList, self).__init__(apiClient, ActiveThingType, url, filters=filters)
+
 
 class DraftThingTypes(RestApiDict):
-
     def __init__(self, apiClient):
         super(DraftThingTypes, self).__init__(
-            apiClient, DraftThingType, DraftIterableThingTypeList, "api/v0002/draft/thing/types" 
-        )      
+            apiClient, DraftThingType, DraftIterableThingTypeList, "api/v0002/draft/thing/types"
+        )
+
 
 class ActiveThingTypes(RestApiDictReadOnly):
-
     def __init__(self, apiClient):
         super(ActiveThingTypes, self).__init__(
-            apiClient, ActiveThingType, ActiveIterableThingTypeList, "api/v0002/thing/types" 
-        )      
+            apiClient, ActiveThingType, ActiveIterableThingTypeList, "api/v0002/thing/types"
+        )
 
 
 # =========================================================================
 # Logical Interfaces for the Thing Type
 # =========================================================================
-        
+
+
 class IterableLogicalInterfaceList(IterableSimpleList):
     def __init__(self, apiClient, url, filters=None):
         # This API does not support sorting
-        super(IterableLogicalInterfaceList, self).__init__(
-            apiClient, BaseLogicalInterface, url
-        )
+        super(IterableLogicalInterfaceList, self).__init__(apiClient, BaseLogicalInterface, url)
+
 
 class DraftLogicalInterfaces(RestApiDict):
     def __init__(self, apiClient, thingTypeId):
         url = "api/v0002/draft/thing/types/%s/logicalinterfaces" % thingTypeId
-        super(DraftLogicalInterfaces, self).__init__(
-            apiClient, 
-            BaseLogicalInterface, 
-            IterableLogicalInterfaceList, 
-            url
-        )
-        
+        super(DraftLogicalInterfaces, self).__init__(apiClient, BaseLogicalInterface, IterableLogicalInterfaceList, url)
+
+
 class ActiveLogicalInterfaces(RestApiDict):
     def __init__(self, apiClient, thingTypeId):
         url = "api/v0002/thing/types/%s/logicalinterfaces" % thingTypeId
         super(ActiveLogicalInterfaces, self).__init__(
-            apiClient, 
-            BaseLogicalInterface, 
-            IterableLogicalInterfaceList,
-            url
-        )        
-                
+            apiClient, BaseLogicalInterface, IterableLogicalInterfaceList, url
+        )
+
+
 # =========================================================================
 # Mappings for the Thing Type
 # =========================================================================
-        
+
 # define the common properties found on most Rest API Items
 class ThingTypeMapping(defaultdict):
     def __init__(self, **kwargs):
         dict.__init__(self, **kwargs)
-        
+
     @property
     def logicalInterfaceId(self):
-        return self["logicalInterfaceId"]   
-        
+        return self["logicalInterfaceId"]
+
     @property
     def notificationStrategy(self):
-        return self["notificationStrategy"]   
-        
+        return self["notificationStrategy"]
+
     @property
     def propertyMappings(self):
-        return self["propertyMappings"]   
-            
+        return self["propertyMappings"]
+
     @property
     def version(self):
-        return self["version"]   
-        
+        return self["version"]
+
     @property
     def created(self):
         return iso8601.parse_date(self["created"])
@@ -220,30 +215,21 @@ class ThingTypeMapping(defaultdict):
     @property
     def updatedBy(self):
         return self["updatedBy"]
-    
+
+
 class IterableMappingList(IterableSimpleList):
     def __init__(self, apiClient, url, filters=None, passApiClient=False):
         # This API does not support sorting
-        super(IterableMappingList, self).__init__(
-            apiClient, ThingTypeMapping, url
-        )
+        super(IterableMappingList, self).__init__(apiClient, ThingTypeMapping, url)
+
 
 class DraftMappings(RestApiDict):
     def __init__(self, apiClient, thingTypeId):
         url = "api/v0002/draft/thing/types/%s/mappings" % thingTypeId
-        super(DraftMappings, self).__init__(
-            apiClient, 
-            ThingTypeMapping, 
-            IterableMappingList, 
-            url
-        )
-        
+        super(DraftMappings, self).__init__(apiClient, ThingTypeMapping, IterableMappingList, url)
+
+
 class ActiveMappings(RestApiDict):
     def __init__(self, apiClient, thingTypeId):
         url = "api/v0002/draft/thing/types/%s/mappings" % thingTypeId
-        super(ActiveMappings, self).__init__(
-            apiClient, 
-            ThingTypeMapping, 
-            IterableMappingList,
-            url
-        )                        
+        super(ActiveMappings, self).__init__(apiClient, ThingTypeMapping, IterableMappingList, url)
--- test/test_state_utils.py
+++ test/test_state_utils.py
@@ -18,26 +18,27 @@ import json
 from wiotp.sdk.api.registry.devices import DeviceUid, DeviceInfo, DeviceCreateRequest, DeviceLocation
 import sys
 
+
 class TestStateUtils:
-    
-    def deleteThingTypes(appClient, TTNameList):           
+    def deleteThingTypes(appClient, TTNameList):
         # delete any left over device types
         for tt in appClient.state.active.thingTypes:
-            #print("Device type instance: %s" % (dt))
+            # print("Device type instance: %s" % (dt))
             if tt.id in TTNameList:
                 for thing in tt.things:
                     print("Deleting things %s for active thing type instance: %s" % (thing.thingId, tt.id))
-                    del tt.devices[thing.thingId]   
+                    del tt.devices[thing.thingId]
                 print("Deactivating old test thing type instance: %s" % (tt.id))
                 appClient.state.active.thingTypes[tt.id].deactivate()
         for tt in appClient.state.draft.thingTypes:
-            #print("Device type instance: %s" % (dt))
+            # print("Device type instance: %s" % (dt))
             if tt.id in TTNameList:
                 for thing in tt.things:
                     print("Deleting things %s for draft thing type instance: %s" % (thing.thingId, tt.id))
-                    del tt.devices[thing.thingId]   
+                    del tt.devices[thing.thingId]
                 print("Deleting old test thing type instance: %s" % (tt.id))
                 del appClient.state.draft.thingTypes[tt.id]
+
 
 class TestStateUtils:
     def deleteDeviceTypes(appClient, DTNameList):
@@ -103,25 +104,23 @@ class TestStateUtils:
         assert deviceType.metadata == metadata
         assert deviceType.edgeConfiguration == edgeConfiguration
         assert deviceType.classId == classId
-        
-    def checkTT (thingType, id, name, description,schemaId, metadata = None):
+
+    def checkTT(thingType, id, name, description, schemaId, metadata=None):
         assert thingType.id == id
         assert thingType.name == name
         assert thingType.description == description
         assert thingType.schemaId == schemaId
         assert thingType.metadata == metadata
-        
-    def checkThing (thing, thingTypeId, thingId , name, description, aggregatedObjects, metadata = None):
+
+    def checkThing(thing, thingTypeId, thingId, name, description, aggregatedObjects, metadata=None):
         assert thing.thingTypeId == thingTypeId
         assert thing.thingId == thingId
         assert thing.description == description
         assert thing.name == name
         assert thing.metadata == metadata
         assert thing.aggregatedObjects == aggregatedObjects
-        
-        
-        
-    def checkMapping (mapping, logicalInterfaceId, notificationStrategy, propertyMappings, version="draft"):
+
+    def checkMapping(mapping, logicalInterfaceId, notificationStrategy, propertyMappings, version="draft"):
         # print("Checking Device Type: %s" % (deviceType))
         assert mapping.logicalInterfaceId == logicalInterfaceId
         assert mapping.notificationStrategy == notificationStrategy
@@ -163,20 +162,20 @@ class TestStateUtils:
             if dt.id == name:
                 return True
         return False
-    
-    def doesTTNameExist (appClient, name):
+
+    def doesTTNameExist(appClient, name):
         for tt in appClient.state.draft.thingTypes.find({"id": name}):
-            if (tt.id == name):
+            if tt.id == name:
                 return True
         return False
-    
-    def doesThingIdExist (appClient,thingTypeId, name):
+
+    def doesThingIdExist(appClient, thingTypeId, name):
         for thing in appClient.state.active.thingTypes[thingTypeId].things:
-            if (thing.thingId == name):
+            if thing.thingId == name:
                 return True
         return False
-    
-    def doesActiveSchemaNameExist (appClient, name):
+
+    def doesActiveSchemaNameExist(appClient, name):
         for a in appClient.state.active.schemas.find({"name": name}):
             if a.name == name:
                 return True
@@ -205,13 +204,13 @@ class TestStateUtils:
             if dt.id == name:
                 return True
         return False
-    
-    def doesActiveTTNameExist (appClient, name):
+
+    def doesActiveTTNameExist(appClient, name):
         for tt in appClient.state.active.thingTypes.find({"id": name}):
-            if (tt.id == name):
+            if tt.id == name:
                 return True
         return False
-    
+
     def createSchema(appClient, name, schemaFileName, schemaContents, description):
         jsonSchemaContents = json.dumps(schemaContents)
         createdSchema = appClient.state.draft.schemas.create(name, schemaFileName, jsonSchemaContents, description)
@@ -253,14 +252,21 @@ class TestStateUtils:
         }
         createdDT = appClient.state.active.deviceTypes.create(payload)
         return createdDT
-    
-    def createTT(appClient,  id, name, description, schemaId, metadata = None):
-        payload = {"id": id, "name": name, 'description' : description, 'schemaId' : schemaId, 'metadata': metadata}
+
+    def createTT(appClient, id, name, description, schemaId, metadata=None):
+        payload = {"id": id, "name": name, "description": description, "schemaId": schemaId, "metadata": metadata}
         createdDT = appClient.state.draft.thingTypes.create(payload)
         return createdDT
-     
-    def createThing(appClient, thingTypeId, thingId, name, description, aggregatedObjects, metadata = None):
-        payload = {"thingTypeId": thingTypeId, "thingId": thingId, "name": name, 'description' : description, 'aggregatedObjects' : aggregatedObjects, 'metadata': metadata}
+
+    def createThing(appClient, thingTypeId, thingId, name, description, aggregatedObjects, metadata=None):
+        payload = {
+            "thingTypeId": thingTypeId,
+            "thingId": thingId,
+            "name": name,
+            "description": description,
+            "aggregatedObjects": aggregatedObjects,
+            "metadata": metadata,
+        }
         createdThing = appClient.state.active.thingTypes[thingTypeId].things.create(payload)
         return createdThing
 
