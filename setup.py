37c2c4c9157333957d686262e382975ecaa3d0b9
David Parker <parkerda@uk.ibm.com>
2019-11-20 09:08:57 +0000

    Prepare for 0.10 release

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ except:
 
 setup(
     name='wiotp-sdk',
-    version="0.9.5",
+    version="0.10.0",
     author='David Parker',
     author_email='parkerda@uk.ibm.com',
     package_dir={'': 'src'},
