abc9a26a5e7d3a446eb65bedc794c115d3b7cf65
David Parker <parkerda@uk.ibm.com>
2024-10-16 16:01:06 +0100

    [patch] Introduce automatic versioning

--- setup.py
+++ setup.py
@@ -25,9 +25,24 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+# Maintain a single source of versioning
+# https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 setup(
     name='wiotp-sdk',
-    version="1.0.0",
+    version=get_version("src/wiotp/sdk/__init__.py"),
     author='David Parker',
     author_email='parkerda@uk.ibm.com',
     package_dir={'': 'src'},
